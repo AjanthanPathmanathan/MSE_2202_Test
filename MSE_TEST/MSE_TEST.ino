f3ca8bbf85a14e7e90608cafe07db894156aba64
AjanthanPathmanathan <80966703+AjanthanPathmanathan@users.noreply.github.com>
2021-03-19 16:05:50 -0400

    Update MSE_TEST.ino

--- MSE_TEST/MSE_TEST.ino
+++ MSE_TEST/MSE_TEST.ino
@@ -1,9 +1,31 @@
+//MSE 2202 Test
+//Ajanthan Pathmanathan - 251127801
+
+// Pot Setup
+int potPin = 36;
+unsigned long potVal = 0;
+
+// Ultrasonic Setup
+int trigPin = 32;
+int echoPin = 34;
+long duration;
+long distance;
+long safetyDistance;
+
 void setup() {
   // put your setup code here, to run once:
-
+  pinMode(36, INPUT_PULLUP);
+  
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  duration = pulseIn(echoPin, HIGH);
 
+  distance = duration*0.034/2;
+  
+  
 }
